32a9b50485bf53cccd875c8593b302daed7c39a7
Shane Williams <shane.williams@resourcekraft.com>
2024-08-27 21:34:05 +0100

    Fix bufferline

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -63,28 +63,86 @@ return {
   -- filename
   {
     "b0o/incline.nvim",
-    dependencies = {},
-    event = "BufReadPre",
-    priority = 1200,
+    event = {
+      "BufReadPre",
+      "BufNewFile",
+    },
+    dependencies = { "mini.icons" },
     config = function()
-      local helpers = require("incline.helpers")
       require("incline").setup({
         window = {
           padding = 0,
           margin = { horizontal = 0 },
         },
         render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
-          local modified = vim.bo[props.buf].modified
-          local buffer = {
-            ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
-            " ",
-            { filename, gui = modified and "bold,italic" or "bold" },
-            " ",
-            guibg = "#363944",
+          local lazy_icons = LazyVim.config.icons
+          local mini_icons = require("mini.icons")
+
+          local function get_filename()
+            local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+            if filename == "" then
+              filename = "[No Name]"
+            end
+            local ft_icon, ft_color = mini_icons.get("file", filename)
+            local modified = vim.bo[props.buf].modified
+            return {
+              " ",
+              { filename, gui = modified and "bold,italic" or "bold" },
+              " ",
+              ft_icon and { ft_icon, " ", guibg = "none", group = ft_color } or "",
+            }
+          end
+
+          local function get_diagnostics()
+            local icons = {
+              error = lazy_icons.diagnostics.Error,
+              warn = lazy_icons.diagnostics.Warn,
+              info = lazy_icons.diagnostics.Info,
+              hint = lazy_icons.diagnostics.Hint,
+            }
+            local labels = {}
+
+            for severity, icon in pairs(icons) do
+              local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+              if n > 0 then
+                table.insert(labels, { " " .. icon .. n, group = "DiagnosticSign" .. severity })
+              end
+            end
+            if #labels > 0 then
+              table.insert(labels, { " ┊" })
+            end
+            return labels
+          end
+
+          local function get_mini_diff()
+            local icons = {
+              add = lazy_icons.git.added,
+              change = lazy_icons.git.modified,
+              delete = lazy_icons.git.removed,
+            }
+            local signs = vim.b[props.buf].minidiff_summary
+
+            local labels = {}
+            if signs == nil then
+              return labels
+            end
+            for name, icon in pairs(icons) do
+              if tonumber(signs[name]) and signs[name] > 0 then
+                table.insert(labels, { " ", icon .. signs[name], group = "MiniDiffSign" .. name })
+              end
+            end
+            if #labels > 0 then
+              table.insert(labels, { " 󰊢 " .. signs.n_ranges .. " ┊" })
+            end
+            return labels
+          end
+
+          return {
+            { get_diagnostics() },
+            { get_mini_diff() },
+            { get_filename() },
+            group = props.focused and "ColorColumn" or "SignColumn",
           }
-          return buffer
         end,
       })
     end,
